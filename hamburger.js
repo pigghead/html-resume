e074afbbcbf95e8bb271ded8a3b706d40ee6c765
Justin Armstrong <justin.strongarms10@gmail.com>
2025-09-24 05:15:35 -0400

    Animated hamburger + side nav menu

--- hamburger.js
+++ hamburger.js
@@ -1,5 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('hamburger').addEventListener('click', function() {
+    const hamburger = document.getElementById('hamburger');
+    const sideWrapper = document.querySelector('.side-wrapper');
+
+    hamburger.addEventListener('click', function(e) {
+        e.stopPropagation(); // Prevent the click from propagating to the window
         this.classList.toggle('active');
+        sideWrapper.classList.toggle('active');
+    });
+
+    sideWrapper.addEventListener('click', function(e) {
+        e.stopPropagation(); // Prevent the click from propagating to the window
+    });
+
+    window.addEventListener('click', function(event) {  
+    if (!event.target.closest('.side-wrapper') && !event.target.closest('#hamburger')) {   
+        hamburger.classList.remove('active');
+        sideWrapper.classList.remove('active');
+    }
+
     });
 });
