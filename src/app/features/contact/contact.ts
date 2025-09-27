55a6b591b0b403fe54374c0f854bfdf64bd856e5
Justin Armstrong <justin.strongarms10@gmail.com>
2025-09-27 11:46:22 -0400

    contact page design/ styling; contact form started

--- src/app/features/contact/contact.ts
+++ src/app/features/contact/contact.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ContactForm } from '../contact-form/contact-form';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ContactForm],
   templateUrl: './contact.html',
   styleUrl: './contact.scss'
 })
