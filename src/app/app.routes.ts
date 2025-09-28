f4577e94f458be37b4478a71b6ed2abf06dd3e3e
Justin Armstrong <justin.strongarms10@gmail.com>
2025-09-28 19:19:11 -0400

    resume page + styling added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home';
 import { AboutComponent } from './features/about/about';
 import { ProjectsComponent } from './features/projects/projects';
+import { Resume } from './features/resume/resume';
 import { ContactComponent } from "./features/contact/contact";
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'projects', component: ProjectsComponent },
+  { path: 'resume', component: Resume },
   { path: 'contact', component: ContactComponent }
 ];
