df2ed51444f629c27c7f639e927b90e03a99d3c7
Justin Armstrong <justin.strongarms10@gmail.com>
2025-10-02 13:42:59 -0400

    implemented a projects service for caching + separating project detail json

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -8,6 +9,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
+    provideHttpClient(withFetch()),
     provideRouter(routes), provideClientHydration(withEventReplay())
   ]
 };
---
+++ src/app/features/projects/projects.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, httpResource } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs';
+
+export interface Skill {name: string; category: string;}
+export interface Project {
+    id: number;
+    name: string;
+    subtitle?: string;
+    date?: string;
+    image?: string;
+    modalImages?: string[];
+    description: string;
+    extendedDesc: string;
+    skills?: Skill[];
+    githubLink?: string;
+}
+
+@Injectable({
+    providedIn: 'root',
+})
+export class ProjectsService {
+    private cache: Project[] | null = null;
+    constructor(private http: HttpClient) {};
+    //Projects: Project[] = []
+
+    getProjects(forceReload = false): Observable<Project[]> {
+        if(this.cache && !forceReload) {
+            return of(this.cache)
+        }
+
+        return this.http.get<Project[]>('/projects.json').pipe(
+            tap(data => this.cache = data)
+        );
+    }
+}
